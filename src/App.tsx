4bf7131455e1b405159ec4deadb1c81dae05ca77
ArchitJ6 <architjain1417@gmail.com>
2024-12-22 20:15:55 +0530

    Final Deployment!

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+function App() {
+  return (
+    <>
+    <div className="cover">
+      <div className="arcReactor">
+        <div className="triangle"></div>
+        <div className="component-1">
+          <span className="innerComponent"></span>
+          <span className="innerComponent"></span>
+          <span className="innerComponent"></span>
+          <span className="innerComponent"></span>
+        </div>
+        <div className="component-2">
+          <span className="innerComponent"></span>
+          <span className="innerComponent"></span>
+          <span className="innerComponent"></span>
+          <span className="innerComponent"></span>
+          <span className="innerComponent"></span>
+          <span className="innerComponent"></span>
+          <span className="innerComponent"></span>
+          <span className="innerComponent"></span>
+        </div>
+        <div className="component-3"></div>
+        <div className="component-4">
+          <span className="innerComponent"></span>
+          <span className="innerComponent"></span>
+          <span className="innerComponent"></span>
+        </div>
+        <div className="component-5">
+          <span className="innerComponent"></span>
+          <span className="innerComponent"></span>
+          <span className="innerComponent"></span>
+        </div>
+        <div className="component-6"></div>
+        <div className="component-7"></div>
+        <div className="component-8">
+          <span className="innerComponent"></span>
+          <span className="innerComponent"></span>
+          <span className="innerComponent"></span>
+        </div>
+      </div></div>
+    </>
+  );
+}
+
+export default App;
